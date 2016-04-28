aaf158d34d2a6fa5c6e75d9f4d2d835bf29e6ad3
James Tranovich <jtranovich@gmail.com>
2016-04-27 18:57:20 -0700

    Migrated to VS2015 and added Kinect20.lib.

--- KinectServer/main.cpp
+++ KinectServer/main.cpp
@@ -1,5 +1,10 @@
 #include <Kinect.h>
 
 int main(int argc, char *argv[]) {
-	return 0;
+    // C++-14 test
+    auto func = [](auto input) { return input * input; };
+    
+    IKinectSensor *sensor = NULL;
+    GetDefaultKinectSensor(&sensor);
+    return 0;
 }
