2d8ca5867cef55c766981fd88d7879bee6b21bb3
James Tranovich <jtranovich@gmail.com>
2016-05-10 19:51:01 -0700

    ember-concurrency to handle websocket and output.

--- KinectClient/app/components/kinect-listener.js
+++ KinectClient/app/components/kinect-listener.js
@@ -1,23 +1,37 @@
 import Ember from 'ember';
+import { task, timeout } from 'ember-concurrency';
 import KinectBodyData from 'kinect-client/utils/kinect-body-data';
 
 const KinectOutputBuilder = ProtoBuf.loadJson(KinectBodyData);
 const KinectOutput = KinectOutputBuilder.build('KBKinectBodies');
+const socketAddress = 'ws://10.0.0.170:8008/';
 
 export default Ember.Component.extend({
 
   socketService: Ember.inject.service('websockets'),
   socket: null,
 
-  willRender() {
-    const socket = this.get('socketService').socketFor('ws://10.0.0.170:8008/');
+  connect: task(function* () {
+    const socket = this.get('socketService').socketFor(socketAddress);
 
     socket.on('open', this.open, this);
     socket.on('message', this.message, this);
     socket.on('close', this.close, this);
 
     this.set('socket', socket);
+  }).on('init'),
 
+  reconnect: task(function* () {
+    yield timeout(2500);
+    this.get('socket').reconnect();
+  }),
+
+  updateData: task(function* (result) {
+    this.set('result', result);
+    yield timeout(1000);
+  }).drop(),
+
+  willRender() {
     // set up enum lookups
     const toArray = (what) => KinectOutputBuilder.lookup(what).children.sortBy('id').mapBy('name');
     const JointType = toArray('KBJointType');
@@ -38,8 +52,8 @@ export default Ember.Component.extend({
     socket.off('close', this.close);
   },
 
-  open(event) {
-    console.log(`On open event has been called: ${event}`);
+  open() {
+    // no-op
   },
 
   message(event) {
@@ -48,12 +62,12 @@ export default Ember.Component.extend({
     fr.onload = (e) => {
       let buffer = new Uint8Array(e.target.result);
       let result = KinectOutput.decode(buffer);
-      this.set('result', result);
+      this.get('updateData').perform(result);
     };
     fr.readAsArrayBuffer(blob);
   },
 
-  close(event) {
-    console.log(`On close event has been called: ${event}`);
+  close() {
+    this.get('reconnect').perform();
   }
 });
