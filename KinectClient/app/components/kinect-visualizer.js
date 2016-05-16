8074d0c2235da45a9ea28af9171e3be34bb826df
James Tranovich <jtranovich@gmail.com>
2016-05-15 19:14:54 -0700

    KinectClient: example Pixi code.

--- KinectClient/app/components/kinect-visualizer.js
+++ KinectClient/app/components/kinect-visualizer.js
@@ -1,11 +1,25 @@
-import Ember from 'ember';
-//import Pixi from 'pixi';
+import PIXI from 'pixi';
+import PixiCanvas from 'ember-cli-pixijs/components/pixi-canvas';
 
-export default Ember.Component.extend({
+export default PixiCanvas.extend({
 
-  //shapes: new Pixi.Container(),
+  shapes: new PIXI.Container(),
 
-  willRender() {
+  init() {
+    this._super(arguments);
+    let test = new PIXI.Graphics();
+    test.position.x = 50;
+    test.position.y = 60;
+    test.lineStyle(1, 0xdedede);
+    test.drawRect(20, 20, 150, 150);
+    test.beginFill(0xff0000);
+    let shapes = this.get('shapes');
+    shapes.addChild(test);
+  },
 
+  draw() {
+    let renderer = this.get('pixiRenderer');
+    let shapes = this.get('shapes');
+    renderer.render(shapes);
   }
 });
